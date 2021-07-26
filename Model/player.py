a01a22b8af9d444420197c00b7b091a168075423
Call-X <emile.miath.architecte@hotmail.com>
2021-07-26 18:17:04 +0200

    shaping

---
+++ Model/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, familyname, lastname, age, ranking):
+        self.familyname = familyname
+        self.lastname = lastname
+        self.age = age
+        self.ranking = ranking
---
+++ main.py
@@ -0,0 +1,10 @@
+from Model import player
+
+
+Player1 = player.Player("bogo", "kevin", 12, "12ème")
+print("Je m'appel " + Player1.familyname)
+
+PlayerE = player.Player("bogo", "kevin", 32, "12ème",)
+print("Je suis classé " + Player1.ranking)
+
+
