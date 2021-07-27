fe4c0ac0ac85cbdfe9a6f9473ec4b3cd4d269a55
Call-X <emile.miath.architecte@hotmail.com>
2021-07-27 14:39:23 +0200

    shaping

--- Model/player.py
+++ Model/player.py
@@ -1,6 +1,7 @@
 class Player:
     def __init__(self, family_name, fisrt_name, age, ranking):
         self.family_name = family_name
-        self.last_name = fisrt_name
+        self.first_name = fisrt_name
         self.age = age
         self.ranking = ranking
+
---
+++ Model/playerlist.py
@@ -0,0 +1,3 @@
+class Playerlist(list):
+    def append(self, player):
+        return super().append(player)
